9e14f3eb9e6251b9e1d5e82afd334726c5f1b3a6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-27 13:42:27 +0000

    build based on a833a1e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LifeWiki","category":"page"},{"location":"#LifeWiki-1","page":"Home","title":"LifeWiki","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LifeWiki]","category":"page"}]
+}
