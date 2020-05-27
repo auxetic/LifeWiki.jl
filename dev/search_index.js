fac33a61f449f90ce97949a7af70cc3fd96c8ba5
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-27 14:08:49 +0000

    build based on ab6a357

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LifeWiki","category":"page"},{"location":"#LifeWiki-1","page":"Home","title":"LifeWiki","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LifeWiki]","category":"page"}]
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LifeWiki","category":"page"},{"location":"#Wiki-of-Life-1","page":"Home","title":"Wiki of Life","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"test","category":"page"},{"location":"#","page":"Home","title":"Home","text":"测试","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LifeWiki]","category":"page"}]
 }
